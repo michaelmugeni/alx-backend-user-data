fde9f6bc271a46e6affcb4feafda1daf44a921a1
michaelmugeni <michaelmugeni98@gmail.com>
2024-11-12 11:05:52 +0000

    added app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,91 +1,22 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-"""Route module for the API.
-"""
-import os
-from os import getenv
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-
-from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-=======
-"""
-Route module for the API
-"""
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
->>>>>>> 59f5f196b3b868767bb0d2e3c31181191d49d143
-
+from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-<<<<<<< HEAD
-auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
-=======
->>>>>>> 59f5f196b3b868767bb0d2e3c31181191d49d143
 
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-<<<<<<< HEAD
-    """Not found handler.
-=======
-    """ Not found handler
->>>>>>> 59f5f196b3b868767bb0d2e3c31181191d49d143
-    """
-    return jsonify({"error": "Not found"}), 404
+app.config.from_object(Config)
 
+babel = Babel(app)
 
-<<<<<<< HEAD
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler.
-    """
-    return jsonify({"error": "Unauthorized"}), 401
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """Forbidden handler.
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def authenticate_user():
-    """Authenticates a user before processing a request.
-    """
-    if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-        ]
-        if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
-                abort(401)
-            if user is None:
-                abort(403)
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-=======
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
->>>>>>> 59f5f196b3b868767bb0d2e3c31181191d49d143
